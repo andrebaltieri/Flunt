41dfc1601dbcf0d836099f6f177ee583bb5bdd67
Emre Savcı <freelancemuhendis@gmail.com>
2018-08-29 00:20:19 +0300

    - added TimeSpanValidationContract - added TimeSpanValidationContract unit tests

---
+++ Flunt/Validations/TimeSpanValidationContract.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Flunt.Validations
+{
+	public partial class Contract
+    {
+		public Contract IsGreaterThan(TimeSpan val, TimeSpan comparer, string property, string message)
+        {
+            if (val <= comparer)
+                AddNotification(property, message);
+
+            return this;
+        }
+
+        public Contract IsGreaterOrEqualsThan(TimeSpan val, TimeSpan comparer, string property, string message)
+        {
+            if (val < comparer)
+                AddNotification(property, message);
+
+            return this;
+        }
+
+        public Contract IsLowerThan(TimeSpan val, TimeSpan comparer, string property, string message)
+        {
+            if (val >= comparer)
+                AddNotification(property, message);
+
+            return this;
+        }
+
+        public Contract IsLowerOrEqualsThan(TimeSpan val, TimeSpan comparer, string property, string message)
+        {
+            if (val > comparer)
+                AddNotification(property, message);
+
+            return this;
+        }
+
+        public Contract IsBetween(TimeSpan val, TimeSpan from, TimeSpan to, string property, string message)
+        {
+            if (!(val > from && val < to))
+                AddNotification(property, message);
+
+            return this;
+        }
+    }
+}
