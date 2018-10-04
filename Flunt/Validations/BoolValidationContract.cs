dc9f575714c9696e89333874132491335bff241e
Igor Rozani <igorrozani@gmail.com>
2018-10-04 09:41:05 -0300

    Code reuse in BoolValidationContract

--- Flunt/Validations/BoolValidationContract.cs
+++ Flunt/Validations/BoolValidationContract.cs
@@ -2,14 +2,6 @@ namespace Flunt.Validations
 {
     public partial class Contract
     {
-        public Contract IsTrue(bool val, string property, string message)
-        {
-            if (!val)
-                AddNotification(property, message);
-
-            return this;
-        }
-
         public Contract IsFalse(bool val, string property, string message)
         {
             if (val)
@@ -17,5 +9,8 @@ namespace Flunt.Validations
 
             return this;
         }
+
+        public Contract IsTrue(bool val, string property, string message) =>
+            IsFalse(!val, property, message);
     }
 }
