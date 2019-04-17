bf4be4ea69026d94f2f60d903bf102243a38129f
Gabriel Domingos <gabrielgdomingos@gmail.com>
2019-04-17 14:26:38 -0300

    Corrigido o between de long
    Corrigido o between de long que não considerava o valor mínimo e máximo dentro do intervalo válido.

--- Flunt/Validations/LongValidationContract.cs
+++ Flunt/Validations/LongValidationContract.cs
@@ -257,7 +257,7 @@
         #region Between      
         public Contract IsBetween(long val, long from, long to, string property, string message)
         {
-            if (!(val > from && val < to))
+            if (!(val >= from && val <= to))
                 AddNotification(property, message);
 
             return this;
