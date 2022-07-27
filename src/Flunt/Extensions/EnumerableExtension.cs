e2d1ecb460cebcb61a3496822ae1d41c74e6236e
Andre Baltieri <andrebaltieri@hotmail.com>
2022-07-27 14:41:49 -0300

    Changed class modifier to internal

--- src/Flunt/Extensions/EnumerableExtension.cs
+++ src/Flunt/Extensions/EnumerableExtension.cs
@@ -1,8 +1,8 @@
 namespace Flunt.Extensions;
 
-public static class EnumerableExtension
+internal static class EnumerableExtension
 {
-    public static bool Contains(this IEnumerable<string> source, string value, StringComparison comparison) 
+    internal static bool Contains(this IEnumerable<string> source, string value, StringComparison comparison)
     {
         var contains = false;
         foreach (var item in source)
