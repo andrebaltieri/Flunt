0b02f9712e290d9e78f1882ab04c7cb681135d40
Andre Baltieri <andrebaltieri@hotmail.com>
2023-04-23 10:19:45 -0300

    Started testing COntains method

--- src/Flunt.Test/Validations/ContainsTest.cs
+++ src/Flunt.Test/Validations/ContainsTest.cs
@@ -5,18 +5,26 @@ namespace Flunt.Test.Validations;
 [TestClass]
 public class ContainsTest
 {
+    private const string Text = "Some Text Here";
+    private int[] _intArray = {1, 2, 3};
+
     [TestMethod]
     public void ShouldContainsAnElement()
     {
         var contract = new Contract()
             .Requires()
-            .Contains(new[] { 1, 2, 3 }, 2)
-            .Contains("testing", 't')
+            // string
+            .Contains(Text, 't')
+
+            // string
+            .Contains(Text, "text", StringComparison.OrdinalIgnoreCase)
+            
+            // Array
+            .Contains(_intArray, 2)
             .Contains("the book is on the table", "book")
-            .Contains<string>(new[] { "apple", "orange", "grape" }, "apple");
+            .Contains<string>(new[] {"apple", "orange", "grape"}, "apple");
     }
 
-    // string
 
     // Array
     // ArrayList
--- src/Flunt/Validations/Contains.cs
+++ src/Flunt/Validations/Contains.cs
@@ -17,10 +17,24 @@ public partial class Contract<TNotification>
     public Contract<TNotification> Contains(
         string text, 
         string value, 
+        StringComparison comparison = StringComparison.OrdinalIgnoreCase,
         string key = "",
         string message = "Text should contains value {1}")
     {
-        if (!text.Contains(value))
+        if (!text.Contains(value, comparison))
+            AddNotification(key, message);
+
+        return this;
+    }
+    
+    public Contract<TNotification> Contains(
+        string text, 
+        char value, 
+        StringComparison comparison = StringComparison.Ordinal,
+        string key = "",
+        string message = "Text should contains value {1}")
+    {
+        if (!text.Contains(value, comparison))
             AddNotification(key, message);
 
         return this;
