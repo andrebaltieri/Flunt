80c343039e77d4a15e2bfc464f9213834bebd073
Andre Baltieri <andrebaltieri@hotmail.com>
2023-04-28 20:50:54 -0300

    Adding support for IsGreaterOrEqual validation

--- src/Flunt/Validations/IsGreaterOrEqualThan.cs
+++ src/Flunt/Validations/IsGreaterOrEqualThan.cs
@@ -1,6 +1,20 @@
 namespace Flunt.Validations;
 
-public class IsGreaterOrEqualThan
+public partial class Contract<TNotification>
 {
-    
+    /// <summary>
+    /// Requires value is greater or equal to comparer
+    /// </summary>
+    public Contract<TNotification> IsGreaterOrEqualThan<TValue>(
+        TValue val, 
+        TValue comparer, 
+        string key = "",
+        string value = "{0} should be greater or equal to {1}")
+        where TValue : IComparable<TValue>
+    {
+        if (val.CompareTo(comparer) == 0)
+            AddNotification(key, string.Format(value, val, comparer));
+
+        return this;
+    }
 }
